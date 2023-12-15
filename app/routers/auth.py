2c3bcd0814cb9117bc6018f02daae3d266766649
MohamedBasueny <mobasueny@gmail.com>
2023-12-15 21:33:17 +0200

    testing

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -19,10 +19,8 @@ async def login(user_credentials : Annotated[OAuth2PasswordRequestForm, Depends(
     user = db.query(User).filter(User.email == user_credentials.username).first()
     
     if not user :
-        print(f"couldn't find a user with this email : {user_credentials.username}")
         return HTTPException(status_code=status.HTTP_403_FORBIDDEN ,
                              detail=f"couldn't find a user with this email : {user_credentials.username}")
-    # print(user.email)
 
     are_passwords_match = verify_passwords(user_credentials.password ,user.password) 
 
