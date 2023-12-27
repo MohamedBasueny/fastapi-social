c687fb725d1025df3548bd581198e0b9bee990e6
MohamedBasueny <mobasueny@gmail.com>
2023-12-28 00:01:32 +0200

    temporary fixing db to fastapi_test

--- app/database.py
+++ app/database.py
@@ -23,7 +23,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 from .config import settings
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:0716103235@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:0716103235@localhost/fastapi_test"
 # SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(
